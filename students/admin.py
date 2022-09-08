0c0794369e03731b972dc79dde5342837e33d19a
dborodin836 <dborodin836@gmail.com>
2022-09-09 01:20:45 +0300

    additional filters and data

--- students/admin.py
+++ students/admin.py
@@ -10,14 +10,27 @@ class ClassAdmin(admin.ModelAdmin):
     def sum_workload(self, obj):
         return sum([x[0] for x in Workload.objects.filter(klass=obj).values_list("workload")])
 
-    sum_workload.short_description = "Сумма нагрузки (Занесённая)"
+    sum_workload.short_description = "Сум. нагрузки (Занесённая)"
 
     def sum_workload_real(self, obj):
         return len(ScheduleItem.objects.filter(workload__klass=obj))
 
-    sum_workload_real.short_description = "Сумма нагрузки (В рассписании)"
+    sum_workload_real.short_description = "Сум. нагрузки (В рассписании)"
 
-    list_display = ("id", "year", "letter_id", "sum_workload", "sum_workload_real")
+    def is_true(self, obj):
+        return self.sum_workload(obj) == self.sum_workload_real(obj)
+
+    is_true.boolean = True
+    is_true.short_description = "Совпадение"
+
+    def name(self, obj):
+        return obj.name
+
+    name.short_description = "Класс"
+
+    list_display = ("id", "name", "sum_workload", "sum_workload_real", "is_true")
+
+    list_filter = ("year", "letter_id")
 
 
 admin.site.register(Class, ClassAdmin)
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.contrib import admin
 
 from lessons.models import ScheduleItem
@@ -14,12 +12,18 @@ class TeacherAdmin(admin.ModelAdmin):
     sum_workload.short_description = "Сумма нагрузки (Занесённая)"
 
     def sum_workload_real(self, obj):
-        return sum([x[0] for x in ScheduleItem.objects.filter(
+        return len([x[0] for x in ScheduleItem.objects.filter(
             workload__teacher=obj).values_list("workload__workload")])
 
     sum_workload_real.short_description = "Сумма нагрузки (В рассписании)"
 
-    list_display = ("id", "name", "sum_workload", "sum_workload_real")
+    def is_true(self, obj):
+        return self.sum_workload(obj) == self.sum_workload_real(obj)
+
+    is_true.boolean = True
+    is_true.short_description = "Совпадение"
+
+    list_display = ("id", "name", "sum_workload", "sum_workload_real", "is_true")
 
 
 class WorkloadAdmin(admin.ModelAdmin):
@@ -28,12 +32,16 @@ class WorkloadAdmin(admin.ModelAdmin):
 
     list_filter = ("lessons", "klass", "teacher",)
 
+    search_fields = ("klass", "teacher", "workload", "lessons")
+
 
 class ScheduleItemAdmin(admin.ModelAdmin):
 
     list_display = ("id", "workload", "lesson_no", "date")
 
     list_filter = ("lesson_no", "date", "workload__teacher", "workload__klass")
+
+    search_fields = ("date", "klass", "teacher", "workload", "lessons")
 
 
 admin.site.register(Teacher, TeacherAdmin)
