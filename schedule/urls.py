82c209efec4a8f59a6da44f66edc0253f7a6096b
dborodin836 <dborodin836@gmail.com>
2022-09-08 23:51:34 +0300

    added second type of schedule

--- schedule/urls.py
+++ schedule/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pages.views.HomeView.as_view()),
     path('full', pages.views.FullSchedule.as_view()),
+    path('full/v2', pages.views.FullScheduleV2.as_view()),
     path('ha', pages.views.generate)
 ]
